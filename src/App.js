a8597fe07aed1adfb92af860fdbc8557912a6931
Matt Windle <matthewwindle@outlook.com>
2021-04-06 12:41:26 +0100

    added logic for admin and login to the nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import './App.css';
 import Nav from './Components/Nav';
 
-
 function App() {
+
+  const [admin, setAdmin] = useState(false);
+  const [loginStatus, setLoginStatus] = useState(false);
+
+
+
   return (
     <div className="App">
-
-    <Nav />
+    <Nav 
+    admin={admin}
+        loginStatus={loginStatus}
+    />
 
     </div>
   );
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,14 @@
-const Nav = () => {
+const Nav = ({ admin, loginStatus}) => {
     return (
         <nav className="flex flex_align-center flex_space-between">
             <img src="logo" />
             <ul className="flex">
                 <li><a href="">Blog posts</a></li>
-                <li><a href="">Admin</a></li>
+                {admin && <li><a href="">Admin</a></li>}
+                {loginStatus && 
+                    <><li><a href="">Profile</a></li><li><a href="">Logout</a></li></>
+                || 
+                <li><a href="" className="login">Login</a></li>}
             </ul>
         </nav>
     );
